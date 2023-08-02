677636eecfaae681b92e7cb1df025f43fb2f3b17
TaLun2075 <TaLun@DESKTOP-WIN10>
2023-08-02 14:10:30 +0200

    remove unused references

--- Sonos/Controllers/ResetController.cs
+++ Sonos/Controllers/ResetController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
-using SonosUPNPCore.Interfaces;
 
 namespace Sonos.Controllers
 {
