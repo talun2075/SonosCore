1f34b75dbe401556cbcc5757aa8ccadce0ace9d8
TaLun2075 <TaLun@DESKTOP-WIN10>
2026-03-17 09:23:35 +0100

    fix fisibility

--- Sonos/wwwroot/js/JSControl.js
+++ Sonos/wwwroot/js/JSControl.js
@@ -870,11 +870,9 @@ function ShowPlaylistLyricCurrent() {
     }
 };//done
 //Neue Funktionen für Jquery remove
-function IsVisible(el) {
-    if (!el) return false;
-    if (!el.offsetParent && getComputedStyle(el).position !== 'fixed') return false;
-    const style = getComputedStyle(el);
-    return style.visibility !== 'hidden' && style.opacity !== '0';
+function IsVisible(DomElement) {
+    //Kein OffSetParent keine Sichtbarkeit
+    return DomElement.offsetParent !== null;
 };//done
 function SetVisible(DomElement) {
     DomElement.style.display = "block";
